2d29d41937d8ff7ea3f93563ea92220ee7afa962
jerrywanauspersonal <jerry.wan@live.com>
2023-03-30 11:03:56 +1100

    modified:   .gitignore 	modified:   Cargo.lock 	modified:   Cargo.toml 	modified:   fastly.toml 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,45 @@
 use fastly::ObjectStore;
 use fastly::{Error, Request, Response};
+use md5::{Md5, Digest};
+use hex;
 
 #[fastly::main]
 fn main(req: Request) -> Result<Response, Error> {
-    // Log out which version of the Fastly Service is responding to this request.
-    // This is useful to know when debugging.
-    if let Ok(fastly_service_version) = std::env::var("FASTLY_SERVICE_VERSION") {
-        println!("FASTLY_SERVICE_VERSION: {}", fastly_service_version);
+    
+    // Construct an ObjectStore instance which is connected to the Object Store named `my_store`
+    // 'my_store' is the object store you created on your account  
+    let store =
+        ObjectStore::open("my_store").map(|store| store.expect("ObjectStore exists"))?;
+    
+    return match req.get_path() {
+        "/notfound" => {
+            let entry = store.lookup("readme")?;
+
+            match entry {
+                // Stream the value back to the user-agent.
+                Some(entry) => Ok(Response::from_body(entry)),
+                None => Ok(Response::from_body("Not Found").with_status(404)),
+            }
+        }
+        _ => {
+            //using md5 to hash the request url path
+            let mut md5_hash = Md5::new();
+            let path_string = String::from(req.get_path());
+            println!("path {}", path_string);
+            md5_hash.update(path_string);
+            let finalized_md5hash =md5_hash.finalize();
+            let hashed_path = hex::encode(finalized_md5hash);
+            println!("path in md5 x {}", hashed_path);
+
+            //retrieve the file from object store with the hashed url path
+            let entry = store.lookup(hashed_path.as_str())?;
+
+            match entry {
+                // Stream the value back to the user-agent.
+                Some(entry) => Ok(Response::from_body(entry)),
+                None => Ok(Response::from_body("Not Found").with_status(404)),
+            }
+        }
     }
 
-    /*
-        Construct an ObjectStore instance which is connected to the Object Store named `my-store`
-
-        [Documentation for the ObjectStore open method can be found here](https://docs.rs/fastly/latest/fastly/struct.ObjectStore.html#method.open)
-    */
-    let mut store =
-        ObjectStore::open("my-store").map(|store| store.expect("ObjectStore exists"))?;
-
-    let path = req.get_path();
-    if path == "/readme" {
-        let entry = store.lookup("readme")?;
-
-        return match entry {
-            // Stream the value back to the user-agent.
-            Some(entry) => Ok(Response::from_body(entry)),
-            None => Ok(Response::from_body("Not Found").with_status(404)),
-        };
-    } else {
-        /*
-        Adds or updates the key `hello` in the Object Store with the value `world`.
-
-        Note: Object stores are eventually consistent, this means that the updated value associated
-        with the key may not be available to read from all edge locations immediately and some edge
-        locations may continue returning the previous value associated with the key.
-
-        [Documentation for the insert method can be found here](https://docs.rs/fastly/latest/fastly/struct.ObjectStore.html#method.insert)
-        */
-        store.insert("hello", "world")?;
-
-        /*
-        Retrieve the value associated with the key `hello` in the Object Store.
-        If the key does not exist, then `None` is returned.
-        If the key does exist, then an `Some<Body>` is returned.
-
-        [Documentation for the lookup method can be found here](https://docs.rs/fastly/latest/fastly/struct.ObjectStore.html#method.lookup)
-        */
-        let entry = store.lookup("hello")?;
-
-        return match entry {
-            // Stream the value back to the user-agent.
-            Some(entry) => Ok(Response::from_body(entry)),
-            None => Ok(Response::from_body("Not Found").with_status(404)),
-        };
-    }
 }

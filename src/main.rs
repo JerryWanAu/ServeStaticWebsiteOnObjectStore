e4ca332b9390f8ef00bac9cb3eeea4d10550fa77
JerryWanAu <jwan@fastly.com>
2023-04-06 14:36:13 +1000

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ fn main(req: Request) -> Result<Response, Error> {
         ObjectStore::open("my_store").map(|store| store.expect("ObjectStore exists"))?;
     
     return match req.get_path() {
+        // for testing on local server
         "/readme" => {
             let entry = store.lookup("readme")?;
 
